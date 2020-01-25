ee0687bdc6e1ca6b14d237d7cd107c03bb7526ce
Rebecca Evans <RebeccaEvans@users.noreply.github.com>
2020-01-25 10:56:21 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+// Import packages
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'
+import jwtDecode from 'jwt-decode'
+
+// Resources and custom components
+import './App.css';
+import Content from './content/Content'
+import Footer from './nav/Footer'
+import Header from './nav/Header'
+import Nav from './nav/Nav'
+
+const App = props => {
+  // Declare state variables
+  let [user, setUser] = useState(null)
+
+  // Define any onload actions (e.g., to look for the token)
+  useEffect(() => {
+    console.log('check for token!')
+    decodeToken()
+  }, [])
+
+  // Helper function to update the user (login, signup, and logout use this)
+  const updateUser = newToken => {
+    if (newToken) {
+      // Save the token 
+      localStorage.setItem('userToken', newToken)
+
+      // Update the user with the token's info
+      decodeToken(newToken)
+    }
+    else {
+      setUser(null)
+    }
+  }
+
+  // Helper function to decode existing tokens
+  const decodeToken = existingToken => {
+    let token = existingToken || localStorage.getItem('userToken')
+    console.log('The token is:', token)
+
+    // Decode token
+    if (token) {
+      let decoded = jwtDecode(token)
+
+      // If the token is not decoded or it is expired, NO USER!
+      if (!decoded || Date.now() > decoded.exp * 1000) {
+        console.log('Expired or bad token?')
+        setUser(null)
+      }
+      else {
+        // This is the user data - YAY
+        console.log('YAY! Good token!')
+        setUser(decoded)
+      }
+    } 
+    else {
+      setUser(null)
+    }
+  }
+
+  return (
+    <Router>
+      <div className="App">
+        <Nav user={user} updateUser={updateUser} />
+        <Header />
+        <main>
+          <Content user={user} updateUser={updateUser} />
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/content/pages/Signup.js
@@ -0,0 +1,93 @@
+// Packages
+import React, { useEffect, useState } from 'react'
+import { Redirect } from 'react-router-dom'
+
+const Signup = props => {
+  // Declare and initialize state variables
+  let [email, setEmail] = useState('')
+  let [firstname, setFirstname] = useState('')
+  let [lastname, setLastname] = useState('')
+  let [message, setMessage] = useState('')
+  let [password, setPassword] = useState('')
+  let [profileUrl, setProfileUrl] = useState('')
+
+  // Set message to blank if I'm typing in the form
+  useEffect(() => {
+    setMessage('')
+  }, [firstname, lastname, email, password, profileUrl])
+
+  const handleSubmit = e => {
+    // Prevent default of form submission
+    e.preventDefault()
+
+    // Form the data object
+    let data = {
+      email,
+      firstname, 
+      lastname,
+      password,
+      profileUrl
+    }
+
+    // Send the user sign up data to the server
+    fetch(`${process.env.REACT_APP_SERVER_URL}/auth/signup`, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      response.json().then(result => {
+        if (response.ok) {
+          // I have a token - update the user info
+          props.updateUser(result.token)
+        }
+        else {
+          // Status was something other than 200
+          setMessage(`${response.status} ${response.statusText}: ${result.message}`)
+        }
+      })
+    })
+    .catch(err => {
+      setMessage(`Error: ${err.toString()}`)
+    })
+  }
+
+  // Redirect if there is already someone logged in
+  if (props.user) {
+    return <Redirect to="/profile" />
+  }
+
+  return (
+    <div>
+      <h2>Signup</h2>
+      <span className="red">{message}</span>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>First Name:</label>
+          <input name="firstname" placeholder="Your first name" onChange={e => setFirstname(e.target.value)} />
+        </div>
+        <div>
+          <label>Last Name:</label>
+          <input name="lastname" placeholder="Your last name" onChange={e => setLastname(e.target.value)} />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input type="email" name="email" onChange={e => setEmail(e.target.value)} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" name="password" onChange={e => setPassword(e.target.value)} />
+        </div>
+        <div>
+          <label>Profile Pic URL:</label>
+          <input type="url" name="profileUrl" onChange={e => setProfileUrl(e.target.value)} />
+        </div>
+        <button type="submit">Sign Me Up!</button>
+      </form>
+    </div>
+  )
+}
+
+export default Signup
